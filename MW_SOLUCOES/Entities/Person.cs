cc4000f98a11e466e9c5ea9831c084948fd5c261
MWillian <matheuswno02@gmail.com>
2026-01-28 22:36:50 -0300

    Implementação das verificações necessárias nas entidades Person e Client

---
+++ MW_SOLUCOES/Entities/Person.cs
@@ -0,0 +1,63 @@
+﻿namespace MW_SOLUCOES.Entities;
+
+using MW_SOLUCOES.Exceptions;
+using MW_SOLUCOES.Helpers;
+
+public class Person
+{
+    public Guid Id { get; private set; }
+    public ClientName Name { get; private set; }
+    public int Age { get; private set; }
+    public string CPF { get; private set; } = string.Empty;
+    public string Phone { get; private set; } = string.Empty;
+    public string Email { get; private set; } = string.Empty;
+    public Address Address { get; private set; }
+
+    public Person(Guid id, ClientName name, int age, string cpf, string phone, Address address)
+    {
+        Id = id;
+        Name = name;
+        Age = age;
+        CPF = cpf;
+        Phone = phone;
+        Address = address;
+    }
+    public void UpdateName(string firstName, string lastName)
+    {
+        if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+        {
+            throw new NegocioException("Primeiro ou último nome estão em branco.");
+        }
+        Name.FirstName = firstName;
+        Name.LastName = lastName;
+    }
+    public void UpdateAge(int age)
+    {
+        if (age < 0 || age > 130)
+        {
+            throw new NegocioException("Idade fora dos padrões aceitáveis.");
+        }
+        Age = age;
+    }
+    public void UpdateContactInfo(string email,string phone)
+    {
+        if (!EmailValidationHelper.IsValidEmail(email))
+        {
+            throw new NegocioException("Email inválido.");
+        }
+        Email = email;
+        if (Phone.Length == 13)
+        {
+            Phone = phone;
+        }
+        else
+        {
+            throw new NegocioException("O número de telefone inserido é inválido");
+        }
+    }
+    public void UpdateCpf(string rawCpf)
+    {
+        string cleanCpf = CPFValidationHelper.RemoveFormat(rawCpf);
+        bool validCpf = CPFValidationHelper.IsValid(rawCpf);
+    }
+}
