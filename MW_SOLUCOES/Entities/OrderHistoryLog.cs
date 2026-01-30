17df3b04a81e4e26b2c740e2d27aecf38eca8cde
MWillian <matheuswno02@gmail.com>
2026-01-30 19:57:11 -0300

    Implementação do histórico de movimentações nos pedidos

---
+++ MW_SOLUCOES/Entities/OrderHistoryLog.cs
@@ -0,0 +1,15 @@
+﻿namespace MW_SOLUCOES.Entities;
+
+public class OrderHistorylog
+{
+    public List<string> History { get; private set; } = new List<string>();
+    private DateTime? LastUpdatedAt { get; set; }
+    
+    public void RegisterLog(string fieldModified)
+    {
+        History.Add($"Campo modificado: {fieldModified} - {DateTime.Now}");
+        LastUpdatedAt = DateTime.Now;
+    }
+    
+    public DateTime? GetDate() { return LastUpdatedAt; }
+}
